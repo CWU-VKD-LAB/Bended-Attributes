64b3f2ef651975283350c095c6e9875fc9a6bccc
AlexWorland <alexworland@gmail.com>
2022-06-16 12:19:27 -0700

    restructured project for bended pairs

--- Decicison Tree Generator/pythonProject2/helloWorld.pl
+++
@@ -1,6 +0,0 @@
-% hello world predicate in prolog
-
-- hello(X) :-
-    write('Hello '),
-    write(X),
-    nl.
--- README.md
+++
@@ -1,4 +0,0 @@
-# cs556_project
-
-A collection of programs from CS556 that were built by students.
-
